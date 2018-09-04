a40492e90344eec9f9dd8fc24a671038dc3d7b7e
Esteban Sotillo <westixy@gmail.com>
2018-09-04 08:16:26 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# ChainVerif - Strings
+
+Pack of rules used to be added to the core [ChainVerif](https://github.com/Westixy/ChainVerif)
+
+Actually WIP 
---
+++ rules.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * 
+   */
+  alphanumeric: {
+    control: /^[a-z0-9]+$/i,
+    error: 'need to be alphanumeric [a-z0-9]'
+  }
+
+}
